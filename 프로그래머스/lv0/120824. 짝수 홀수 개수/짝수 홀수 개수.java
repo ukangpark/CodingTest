ee7e9590aabf2ace3e46ae3fde21013d776602c6
Joy <123609487+ukangpark@users.noreply.github.com>
2023-07-24 17:18:15 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] result = new int[2];
+        for (int i=0; i<num_list.length; i++) {
+            if(num_list[i]%2 == 0) {
+                result[0]++;
+            }else {
+                result[1]++;
+            }
+        }
+        return result;
+    }
+}
